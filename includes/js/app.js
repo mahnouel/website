20573d5accc8484e7d1066a13249f36d4007e57d
Manuel <hello@m-k.io>
2018-12-21 15:41:43 +0100

    [FEAT] White Theme, CodeSandbox

--- includes/js/app.js
+++ includes/js/app.js
@@ -1,52 +1,52 @@
 /* global XMLHttpRequest:true */
 
 const RSS_URL =
-  'https://cors-anywhere.herokuapp.com/https://feeds.pinboard.in/json/u:mahnouel/t:mahnouel.io/'
+  "https://cors-anywhere.herokuapp.com/https://feeds.pinboard.in/json/u:mahnouel/t:mahnouel.io/";
 
-function get (url) {
-  return new Promise(function (resolve, reject) {
-    const request = new XMLHttpRequest()
-    request.open('GET', url, true)
+function get(url) {
+  return new Promise(function(resolve, reject) {
+    const request = new XMLHttpRequest();
+    request.open("GET", url, true);
 
-    request.onload = function () {
+    request.onload = function() {
       if (this.status >= 200 && this.status < 400) {
-        resolve(this.response)
+        resolve(this.response);
       } else {
-        reject(new Error(this.statusText))
+        reject(new Error(this.statusText));
       }
-    }
+    };
 
-    request.onerror = function (error) {
-      console.log(error)
-      reject(new Error('Network error'))
-    }
+    request.onerror = function(error) {
+      console.log(error);
+      reject(new Error("Network error"));
+    };
 
-    request.send()
-  })
+    request.send();
+  });
 }
 
 get(RSS_URL).then(data => {
-  const json = JSON.parse(data)
+  const json = JSON.parse(data);
   for (const key in json) {
-    const item = json[key]
+    const item = json[key];
 
-    const link = document.createElement('a')
-    link.classList.add('link')
-    link.setAttribute('href', item.u)
-    link.innerHTML = item.d
+    const link = document.createElement("a");
+    link.classList.add("link");
+    link.setAttribute("href", item.u);
+    link.innerHTML = item.d;
 
-    const description = document.createElement('small')
-    description.classList.add('link__description')
-    description.innerHTML = `${item.n}`
+    const description = document.createElement("small");
+    description.classList.add("link__description");
+    description.innerHTML = `${item.n}`;
 
-    const element = document.createElement('span')
-    element.classList.add('etc__item')
-    element.appendChild(link)
+    const element = document.createElement("span");
+    element.classList.add("etc__item");
+    element.appendChild(link);
     // li.appendChild(document.createElement('br'))
     // element.appendChild(description)
 
-    const container = document.querySelector('.js-etc')
-    container.appendChild(element)
+    const container = document.querySelector(".js-etc");
+    container.appendChild(element);
   }
-  console.log(json)
-})
+  console.log(json);
+});
