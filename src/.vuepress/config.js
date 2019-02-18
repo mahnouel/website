cfd87d6cd690c403f076051cd34d24d82f0216fc
Manuel <hello@m-k.io>
2019-02-18 18:15:19 +0100

    feat; add vuepress

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,19 +1,19 @@
 // .vuepress/config.js
-const extendMarkdown = require('./markdown.js').extendMarkdown;
+const extendMarkdown = require("./markdown.js").extendMarkdown;
 console.log(process.env.NODE_ENV);
 module.exports = {
-  base:
-    process.env.SEMAPHORE_GIT_BRANCH !== 'master' ? `/${process.env.SEMAPHORE_GIT_BRANCH}/` : '',
+  // base:
+  //   process.env.SEMAPHORE_GIT_BRANCH !== 'master' ? `/${process.env.SEMAPHORE_GIT_BRANCH}/` : '',
   themeConfig: {
     // nav: [
     //   { text: 'Home', link: '/bla' },
     //   { text: 'Guide', link: '/guide/' },
     //   { text: 'External', link: 'https://google.com' },
     // ],
-    lastUpdated: 'Last Updated', // string | boolean
+    lastUpdated: "Last Updated" // string | boolean
   },
   markdown: {
-    extendMarkdown,
+    extendMarkdown
   },
-  plugins: ['@vuepress/blog'],
+  plugins: ["@vuepress/plugin-blog"]
 };
