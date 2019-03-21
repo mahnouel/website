b173fefea85b68b846182858dcb22e3f4726009b
Manuel <hello@m-k.io>
2019-03-21 23:27:11 +0100

    refactor: header and footer are now components

---
+++ src/components/MagicFooter.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="footer">
+    <p>
+      <g-link to="/">m-k.io</g-link>
+    </p>
+  </footer>
+</template>
+    
+   
---
+++ src/components/MagicHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="header">
+    <g-link class="header__title" to="/">{{ metaData.siteName }}</g-link>
+  </header>
+</template>
+
+<script>
+export default {
+  props: ["metaData"]
+};
+</script>
