a0ddbd047b5def3298dc2fd334e6ac7f2874ab3c
Manuel <hello@m-k.io>
2019-07-09 17:31:10 +0200

    fix: remove simpleanalytics

--- src/main.js
+++ src/main.js
@@ -5,22 +5,9 @@ import DefaultLayout from "~/layouts/Default.vue";
 import TextLayout from "~/layouts/Text.vue";
 import "~/assets/style/index.styl";
 
-function addPing() {
-  const script = document.createElement("script");
-  script.setAttribute("src", "https://ping.m-k.io/app.js");
-  document.body.appendChild(script);
-
-  const noscript = document.createElement("noscript");
-  const img = document.createElement("img");
-  img.setAttribute("src", "https://ping.m-k.io/image.gif");
-  noscript.appendChild(img);
-  document.body.appendChild(noscript);
-}
-
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
   router.addRoutes([{ path: "/", redirect: "/de/" }]);
   Vue.component("Layout", DefaultLayout);
   Vue.component("TextLayout", TextLayout);
-  addPing();
 }
