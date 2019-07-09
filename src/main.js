4e95fa55d64708ba7317b290a65b0f69c8ad2834
Manuel <hello@m-k.io>
2019-07-09 17:27:53 +0200

    feat: add simpleanalytics support

--- src/main.js
+++ src/main.js
@@ -5,15 +5,22 @@ import DefaultLayout from "~/layouts/Default.vue";
 import TextLayout from "~/layouts/Text.vue";
 import "~/assets/style/index.styl";
 
+function addPing() {
+  const script = document.createElement("script");
+  script.setAttribute("src", "https://ping.m-k.io/app.js");
+  document.body.appendChild(script);
+
+  const noscript = document.createElement("noscript");
+  const img = document.createElement("img");
+  img.setAttribute("src", "https://ping.m-k.io/image.gif");
+  noscript.appendChild(img);
+  document.body.appendChild(noscript);
+}
+
 export default function(Vue, { router, head, isClient }) {
   // Set default layout as a global component
-
-  // [todo] Do something about XY
-  const foobar = ({ foo, bar }) => {
-    return foo + bar;
-  };
-
   router.addRoutes([{ path: "/", redirect: "/de/" }]);
   Vue.component("Layout", DefaultLayout);
   Vue.component("TextLayout", TextLayout);
+  addPing();
 }
