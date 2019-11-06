19201c065c77b59b9c7d6d7182a6ba28dc3539a5
Manuel <hello@m-k.io>
2019-11-06 16:05:21 +0100

    feat: move script injection to netlify

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -20,41 +20,6 @@ export default {
   components: {
     MagicHeader
   },
-  mounted() {
-    (() => {
-      const script = document.createElement("script");
-      script.setAttribute("src", "https://ping.m-k.io/app.js");
-      script.setAttribute("async", true);
-      script.setAttribute("defer", true);
-      document.body.appendChild(script);
-      
-      const linkWebmention = document.createElement("link");
-      linkWebmention.setAttribute("rel", "webmention");
-      linkWebmention.setAttribute("href", "https://webmention.io/m-k.io_de_/webmention");
-      document.head.appendChild(linkWebmention);
-
-      const linkPingback = document.createElement("link");
-      linkPingback.setAttribute("rel", "pingback");
-      linkPingback.setAttribute("href", "https://webmention.io/m-k.io_de_/xmlrpc");
-      document.head.appendChild(linkPingback);
-      
-      const linkEndpoint = document.createElement("link");
-      linkEndpoint.setAttribute("rel", "authorization_endpoint");
-      linkEndpoint.setAttribute("href", "https://indieauth.com/auth");
-      document.head.appendChild(linkEndpoint);
-     
-      const linkTokenEndpoint = document.createElement("link");
-      linkTokenEndpoint.setAttribute("rel", "token_endpoint");
-      linkTokenEndpoint.setAttribute("href", "https://tokens.indieauth.com/token");
-      document.head.appendChild(linkTokenEndpoint);
-
-      const noscript = document.createElement("noscript");
-      const img = document.createElement("img");
-      img.setAttribute("src", "https://ping.m-k.io/image.gif");
-      noscript.appendChild(img);
-      document.body.appendChild(noscript);
-    })();
-  },
   props: ["hideHeader"]
 };
 </script>
