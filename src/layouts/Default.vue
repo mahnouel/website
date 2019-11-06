51bc7b0d9f25af78d1b6d7e8693c466ade4a9b86
Manuel <hello@m-k.io>
2019-11-06 13:16:40 +0100

    feat: add webmention

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -27,6 +27,16 @@ export default {
       script.setAttribute("async", true);
       script.setAttribute("defer", true);
       document.body.appendChild(script);
+      
+      const linkWebmention = document.createElement("link");
+      linkWebmention.setAttribute("rel", "webmention");
+      linkWebmention.setAttribute("href", "https://webmention.io/m-k.io_de_/webmention");
+      document.head.appendChild(linkWebmention);
+
+      const linkPingback = document.createElement("link");
+      linkPingback.setAttribute("rel", "pingback");
+      linkPingback.setAttribute("href", "https://webmention.io/m-k.io_de_/xmlrpc");
+      document.head.appendChild(linkPingback);
 
       const noscript = document.createElement("noscript");
       const img = document.createElement("img");
